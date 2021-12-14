b5456413e34953698f5a8ef5f5d13d6e1eba66f1
Jonathan Lamb <jlamb.8153@gmail.com>
2021-12-14 22:34:25 +0000

    day 10 part 2

--- src/2021/10-syntax-scoring/__tests__/scoring.test.ts
+++ src/2021/10-syntax-scoring/__tests__/scoring.test.ts
@@ -1,9 +1,14 @@
 import { smallInput, largeInput } from '../fixtures';
-import { findSyntaxErrorScore } from '../scoring';
+import { getScores } from '../scoring';
 
 describe('Syntax Scoring', () => {
     it('should find the total syntax error score for illegal characters', () => {
-        expect(findSyntaxErrorScore(smallInput)).toStrictEqual(26397);
-        expect(findSyntaxErrorScore(largeInput)).toStrictEqual(343863);
+        expect(getScores(smallInput).syntaxErrorScore).toStrictEqual(26397);
+        expect(getScores(largeInput).syntaxErrorScore).toStrictEqual(343863);
+    });
+
+    it('should autocomplete unfinished strings and find the middle score', () => {
+        expect(getScores(smallInput).middleScore).toStrictEqual(288957);
+        expect(getScores(largeInput).middleScore).toStrictEqual(2924734236);
     });
 });
--- src/2021/10-syntax-scoring/scoring.ts
+++ src/2021/10-syntax-scoring/scoring.ts
@@ -1,8 +1,15 @@
-const characterToPointsMap = new Map<string, number>([
+const illegalCharacterPoints = new Map<string, number>([
     [')', 3],
     [']', 57],
     ['}', 1197],
     ['>', 25137],
+]);
+
+const autoCompletePoints = new Map<string, number>([
+    ['(', 1],
+    ['[', 2],
+    ['{', 3],
+    ['<', 4],
 ]);
 
 const openChunkSet = new Set(['(', '[', '{', '<']);
@@ -13,22 +20,41 @@ const closeToOpenChunkMap = new Map<string, string>([
     ['>', '<'],
 ]);
 
-export const findSyntaxErrorScore = (input: string) => {
+export const getScores = (input: string) => {
     const lines = input.split('\n');
 
-    let score = 0;
+    let syntaxErrorScore = 0;
+    const autocompleteScores = [];
 
     for (const line of lines) {
         const stack = [];
+        let corrupt = false;
         for (const chunk of line) {
             if (openChunkSet.has(chunk)) stack.push(chunk);
             else if (closeToOpenChunkMap.has(chunk)) {
                 if (stack.pop() !== closeToOpenChunkMap.get(chunk)) {
-                    score += characterToPointsMap.get(chunk);
+                    syntaxErrorScore += illegalCharacterPoints.get(chunk);
+                    corrupt = true;
+                    break;
                 }
             } else throw new Error('Unrecognised chunk');
         }
+
+        if (stack.length !== 0 && !corrupt) {
+            let score = 0;
+            for (let i = stack.length - 1; i >= 0; i--) {
+                score *= 5;
+                score += autoCompletePoints.get(stack[i]);
+            }
+            autocompleteScores.push(score);
+        }
     }
 
-    return score;
+    autocompleteScores.sort((a, b) => b - a);
+
+    return {
+        syntaxErrorScore,
+        middleScore:
+            autocompleteScores[Math.floor(autocompleteScores.length / 2)],
+    };
 };

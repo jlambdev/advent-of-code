970e91fc0dea8a766e5fc73e77a11775068a899c
Jonathan Lamb <jlamb.8153@gmail.com>
2021-12-15 22:18:52 +0100

    set up test harness for advent of code day 12

---
+++ src/2021/12-passage-pathing/__tests__/paths.test.ts
@@ -0,0 +1,11 @@
+import { largeInput, mediumInput, smallInput } from '../fixtures';
+import { countPathsSingleSmallCaveVisit } from '../paths';
+
+describe('Passage Pathing', () => {
+    it('should find the number of paths that visit small caves a maximum of 1 time', () => {
+        expect(countPathsSingleSmallCaveVisit(smallInput)).toStrictEqual(10);
+        expect(countPathsSingleSmallCaveVisit(mediumInput)).toStrictEqual(19);
+        expect(countPathsSingleSmallCaveVisit(largeInput)).toStrictEqual(226);
+        // expect(countPathsSingleSmallCaveVisit(puzzleInput)).toStrictEqual(1);
+    });
+});
---
+++ src/2021/12-passage-pathing/paths.ts
@@ -0,0 +1,1 @@
+export const countPathsSingleSmallCaveVisit = (input: string) => {};

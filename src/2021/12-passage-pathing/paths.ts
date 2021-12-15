ca4819060b30c25764b4bccc8a4a0639caac056a
Jonathan Lamb <jlamb.8153@gmail.com>
2021-12-15 22:24:32 +0100

    comments

--- src/2021/12-passage-pathing/paths.ts
+++ src/2021/12-passage-pathing/paths.ts
@@ -1,1 +1,7 @@
-export const countPathsSingleSmallCaveVisit = (input: string) => {};
+export const countPathsSingleSmallCaveVisit = (input: string) => {
+    // how the hell do you solve this?
+    // 1. build data structure: nodes with connecting neighbours (like many linked lists?)
+    // see how it was done for the octopus one?
+    // go from start and go from 'left most' node
+    // base case: reached end or went into a small cave again, or reached start
+};

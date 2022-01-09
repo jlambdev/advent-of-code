e329c98fc1dc4e0b5e07e5e3d20fea838fb0c6ce
Jonathan Lamb <jlamb.8153@gmail.com>
2022-01-09 02:33:59 +0100

    solve part 2 of day 6, aoc 2020

--- src/2020/06-custom-customs/__tests__/customs.test.ts
+++ src/2020/06-custom-customs/__tests__/customs.test.ts
@@ -1,9 +1,19 @@
-import { sumAnswersForAllGroups } from '../customs';
+import {
+    sumAnswersForAllGroups,
+    sumConsistentAnswersForAllGroups,
+} from '../customs';
 import { smallInput, puzzleInput } from '../fixtures';
 
 describe('Custom Customs', () => {
     it('should get the sum of questions answered `yes` for all groups', () => {
         expect(sumAnswersForAllGroups(smallInput)).toStrictEqual(11);
         expect(sumAnswersForAllGroups(puzzleInput)).toStrictEqual(6335);
+    });
+
+    it('should sum only answers that were given by all group members', () => {
+        expect(sumConsistentAnswersForAllGroups(smallInput)).toStrictEqual(6);
+        expect(sumConsistentAnswersForAllGroups(puzzleInput)).toStrictEqual(
+            3392,
+        );
     });
 });
--- src/2020/06-custom-customs/customs.ts
+++ src/2020/06-custom-customs/customs.ts
@@ -12,3 +12,26 @@ export const sumAnswersForAllGroups = (input: string) => {
 
     return sum;
 };
+
+export const sumConsistentAnswersForAllGroups = (input: string) => {
+    const groups = input.split('\n\n');
+
+    let sum = 0;
+    for (const group of groups) {
+        const answerCounter = new Map<string, number>();
+        const members = group.split('\n');
+
+        members.map((answers) => {
+            answers.split('').map((answer) => {
+                if (!answerCounter.has(answer)) answerCounter.set(answer, 1);
+                else answerCounter.set(answer, answerCounter.get(answer) + 1);
+            });
+        });
+
+        for (const [_, count] of answerCounter) {
+            if (count === members.length) sum += 1;
+        }
+    }
+
+    return sum;
+};

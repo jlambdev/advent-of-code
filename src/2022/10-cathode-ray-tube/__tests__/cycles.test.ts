d9cc4eb4fd51b6c045180c99cd955aca3988cf92
Jonathan Lamb <jlamb.8153@gmail.com>
2022-12-10 19:31:44 +0100

    2022 day 10 part 2

--- src/2022/10-cathode-ray-tube/__tests__/cycles.test.ts
+++ src/2022/10-cathode-ray-tube/__tests__/cycles.test.ts
@@ -1,79 +1,150 @@
 import { smallInput, largeInput, puzzleInput } from '../fixture';
-import { CPU, runProgram, sumOfSignalStrengths } from '../cycles';
+import { CPU, CRT, runProgram, sumOfSignalStrengths } from '../cycles';
+
+const largeInputImage = `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`;
+
+// RGLRBZAU
+const puzzleInputImage = `###...##..#....###..###..####..##..#..#.
+#..#.#..#.#....#..#.#..#....#.#..#.#..#.
+#..#.#....#....#..#.###....#..#..#.#..#.
+###..#.##.#....###..#..#..#...####.#..#.
+#.#..#..#.#....#.#..#..#.#....#..#.#..#.
+#..#..###.####.#..#.###..####.#..#..##..`;
 
 describe('Cathode-Ray Tube', () => {
-    it('creates a CPU with a single register value of `1`', () => {
-        const cpu = new CPU();
-        expect(cpu.registerValue).toStrictEqual(1);
+    describe('CPU', () => {
+        it('creates a CPU with a single register value of `1`', () => {
+            const cpu = new CPU();
+            expect(cpu.register).toStrictEqual(1);
+        });
+
+        it('calls `noop` that queues an empty function in the CPU operation queue', () => {
+            const cpu = new CPU();
+            const initialregister = cpu.register;
+            expect(cpu.clockCycle).toStrictEqual(1);
+
+            cpu.noop();
+            expect(cpu.pendingOperations).toStrictEqual(1);
+
+            cpu.tick();
+            expect(cpu.clockCycle).toStrictEqual(2);
+            expect(cpu.register).toStrictEqual(initialregister);
+            expect(cpu.pendingOperations).toStrictEqual(0);
+        });
+
+        it('calls `addX` that queues a registry value changes after 2 cyles', () => {
+            const cpu = new CPU();
+            const initialregister = cpu.register;
+            expect(cpu.clockCycle).toStrictEqual(1);
+
+            cpu.addX(3);
+            expect(cpu.pendingOperations).toStrictEqual(2);
+
+            cpu.tick();
+            expect(cpu.clockCycle).toStrictEqual(2);
+            expect(cpu.register).toStrictEqual(initialregister);
+            expect(cpu.pendingOperations).toStrictEqual(1);
+
+            cpu.tick();
+            expect(cpu.clockCycle).toStrictEqual(3);
+            expect(cpu.register).toStrictEqual(4);
+            expect(cpu.pendingOperations).toStrictEqual(0);
+        });
     });
 
-    it('calls `noop` that queues an empty function in the CPU operation queue', () => {
-        const cpu = new CPU();
-        const initialRegisterValue = cpu.registerValue;
-        expect(cpu.currentClockCycle).toStrictEqual(1);
+    describe('Program', () => {
+        it('runs a program that loads and executes all the instructions', () => {
+            const emptySet = new Set<number>();
+            const { cpu } = runProgram(smallInput, emptySet);
 
-        cpu.noop();
-        expect(cpu.pendingOperations).toStrictEqual(1);
+            expect(cpu.clockCycle).toStrictEqual(6);
+            expect(cpu.register).toStrictEqual(-1);
+            expect(cpu.pendingOperations).toStrictEqual(0);
+        });
 
-        cpu.tick();
-        expect(cpu.currentClockCycle).toStrictEqual(2);
-        expect(cpu.registerValue).toStrictEqual(initialRegisterValue);
-        expect(cpu.pendingOperations).toStrictEqual(0);
+        it('gets the signal strength of a program at a specific cycle for large programs', () => {
+            const signalStrengthsAt = new Set<number>([20, 60, 100, 140, 180, 220]);
+            const { signalStrengths } = runProgram(largeInput, signalStrengthsAt);
+
+            expect(signalStrengths.get(20)).toStrictEqual(420);
+            expect(signalStrengths.get(60)).toStrictEqual(1140);
+            expect(signalStrengths.get(100)).toStrictEqual(1800);
+            expect(signalStrengths.get(140)).toStrictEqual(2940);
+            expect(signalStrengths.get(180)).toStrictEqual(2880);
+            expect(signalStrengths.get(220)).toStrictEqual(3960);
+        });
+
+        it('gets the sum of all signal strengths for a large program', () => {
+            const signalStrengthsAt = new Set<number>([20, 60, 100, 140, 180, 220]);
+
+            const { signalStrengths: largeInputResult } = runProgram(
+                largeInput,
+                signalStrengthsAt,
+            );
+            expect(sumOfSignalStrengths(largeInputResult)).toStrictEqual(13140);
+
+            const { signalStrengths: puzzleInputResult } = runProgram(
+                puzzleInput,
+                signalStrengthsAt,
+            );
+            expect(sumOfSignalStrengths(puzzleInputResult)).toStrictEqual(14420);
+        });
+
+        it('draws an image using a CRT for large programs', () => {
+            const emptySet = new Set<number>();
+            const { image: largeInputResult } = runProgram(largeInput, emptySet);
+            expect(largeInputResult).toStrictEqual(largeInputImage);
+
+            const { image: puzzleInputResult } = runProgram(puzzleInput, emptySet);
+            expect(puzzleInputResult).toStrictEqual(puzzleInputImage);
+        });
     });
 
-    it('calls `addX` that queues a registry value changes after 2 cyles', () => {
-        const cpu = new CPU();
-        const initialRegisterValue = cpu.registerValue;
-        expect(cpu.currentClockCycle).toStrictEqual(1);
+    describe('CRT', () => {
+        it('should create a CRT that keeps track of the sprite position on a 40 char line', () => {
+            const crt = new CRT();
+            expect(crt.spriteLine).toStrictEqual(
+                '###.....................................',
+            );
 
-        cpu.addX(3);
-        expect(cpu.pendingOperations).toStrictEqual(2);
+            crt.drawSpriteOnLine(0);
+            expect(crt.spriteLine).toStrictEqual(
+                '##......................................',
+            );
 
-        cpu.tick();
-        expect(cpu.currentClockCycle).toStrictEqual(2);
-        expect(cpu.registerValue).toStrictEqual(initialRegisterValue);
-        expect(cpu.pendingOperations).toStrictEqual(1);
+            crt.drawSpriteOnLine(16);
+            expect(crt.spriteLine).toStrictEqual(
+                '...............###......................',
+            );
 
-        cpu.tick();
-        expect(cpu.currentClockCycle).toStrictEqual(3);
-        expect(cpu.registerValue).toStrictEqual(4);
-        expect(cpu.pendingOperations).toStrictEqual(0);
-    });
+            crt.drawSpriteOnLine(39);
+            expect(crt.spriteLine).toStrictEqual(
+                '......................................##',
+            );
+        });
 
-    it('runs a program that loads and executes all the instructions', () => {
-        const emptySet = new Set<number>();
-        const { cpu } = runProgram(smallInput, emptySet);
+        it('should create multi-line image, where the line number is based on the current cycle', () => {
+            const crt = new CRT();
 
-        expect(cpu.currentClockCycle).toStrictEqual(6);
-        expect(cpu.registerValue).toStrictEqual(-1);
-        expect(cpu.pendingOperations).toStrictEqual(0);
-    });
+            crt.drawPixel(39);
+            crt.drawPixel(40);
+            crt.drawPixel(41);
+            crt.drawPixel(42);
+            crt.drawPixel(81);
+            crt.drawPixel(82);
+            crt.drawPixel(130);
+            crt.drawPixel(131);
+            crt.drawPixel(161);
+            crt.drawPixel(162);
+            crt.drawPixel(210);
+            crt.drawPixel(240);
 
-    it('gets the signal strength of a program at a specific cycle for large programs', () => {
-        const signalStrengthsAt = new Set<number>([20, 60, 100, 140, 180, 220]);
-        const { signalStrengths } = runProgram(largeInput, signalStrengthsAt);
-
-        expect(signalStrengths.get(20)).toStrictEqual(420);
-        expect(signalStrengths.get(60)).toStrictEqual(1140);
-        expect(signalStrengths.get(100)).toStrictEqual(1800);
-        expect(signalStrengths.get(140)).toStrictEqual(2940);
-        expect(signalStrengths.get(180)).toStrictEqual(2880);
-        expect(signalStrengths.get(220)).toStrictEqual(3960);
-    });
-
-    it('gets the sum of all signal strengths for a large program', () => {
-        const signalStrengthsAt = new Set<number>([20, 60, 100, 140, 180, 220]);
-
-        const { signalStrengths: largeInputResult } = runProgram(
-            largeInput,
-            signalStrengthsAt,
-        );
-        expect(sumOfSignalStrengths(largeInputResult)).toStrictEqual(13140);
-
-        const { signalStrengths: puzzleInputResult } = runProgram(
-            puzzleInput,
-            signalStrengthsAt,
-        );
-        expect(sumOfSignalStrengths(puzzleInputResult)).toStrictEqual(14420);
+            expect(crt.image).toStrictEqual('..\n##\n##\n..\n##\n..');
+        });
     });
 });
--- src/2022/10-cathode-ray-tube/cycles.ts
+++ src/2022/10-cathode-ray-tube/cycles.ts
@@ -1,20 +1,13 @@
 export class CPU {
-    private register: number;
-    private clockCycle: number;
-    private operationQueue: Array<() => void>;
+    register: number;
+    clockCycle: number;
+
+    private readonly operationQueue: Array<() => void>;
 
     constructor() {
         this.register = 1;
         this.clockCycle = 1;
         this.operationQueue = [];
-    }
-
-    get registerValue(): number {
-        return this.register;
-    }
-
-    get currentClockCycle(): number {
-        return this.clockCycle;
     }
 
     get pendingOperations(): number {
@@ -36,6 +29,35 @@ export class CPU {
     }
 }
 
+export class CRT {
+    spriteLine: string; // 40 char string
+
+    private screen: Array<Array<string>>; // 0 - 6 rows of arrays of chars
+
+    constructor() {
+        this.drawSpriteOnLine(1);
+        this.screen = [...Array(6)].map((_) => []);
+    }
+
+    drawSpriteOnLine(spritePosition: number): void {
+        const line: Array<string> = [];
+        for (let i = 0; i < 40; i++) {
+            const pixel = Math.abs(spritePosition - i) <= 1 ? '#' : '.';
+            line.push(pixel);
+        }
+        this.spriteLine = line.join('');
+    }
+
+    drawPixel(currentCycle: number): void {
+        const rowNumber = Math.floor((currentCycle - 1) / 40);
+        this.screen[rowNumber].push(this.spriteLine[(currentCycle - 1) % 40]);
+    }
+
+    get image(): string {
+        return this.screen.map((row) => row.join('')).join('\n');
+    }
+}
+
 function isNoop(instruction: string): boolean {
     return instruction.length === 4;
 }
@@ -46,9 +68,11 @@ export function runProgram(
 ): {
     cpu: CPU;
     signalStrengths: Map<number, number>;
+    image: string;
 } {
     const cpu = new CPU();
     const signalStrengths = new Map<number, number>();
+    const crt = new CRT();
 
     input.split('\n').forEach((instruction) => {
         if (isNoop(instruction)) {
@@ -60,18 +84,18 @@ export function runProgram(
     });
 
     while (cpu.pendingOperations > 0) {
+        crt.drawPixel(cpu.clockCycle);
         cpu.tick();
-        if (signalStrengthsAt.has(cpu.currentClockCycle)) {
-            signalStrengths.set(
-                cpu.currentClockCycle,
-                cpu.currentClockCycle * cpu.registerValue,
-            );
+        if (signalStrengthsAt.has(cpu.clockCycle)) {
+            signalStrengths.set(cpu.clockCycle, cpu.clockCycle * cpu.register);
         }
+        crt.drawSpriteOnLine(cpu.register);
     }
 
     return {
         cpu,
         signalStrengths,
+        image: crt.image,
     };
 }
 

00020023760fe75d188357ff002b48d57760c359
Jonathan Lamb <jlamb.8153@gmail.com>
2022-12-19 23:24:26 +0000

    2022 day 13 both parts

--- src/2022/13-distress-signal/__tests__/signals.test.ts
+++ src/2022/13-distress-signal/__tests__/signals.test.ts
@@ -1,9 +1,56 @@
 import { smallInput, puzzleInput } from '../fixture';
-import { sumOfIndicesOfPairsInCorrectOrder } from '../signals';
+import {
+    compare,
+    findDecoderKey,
+    parseLine,
+    sumOfIndicesOfPairsInCorrectOrder,
+} from '../signals';
 
 describe('Distress Signal', () => {
-    it.skip('should return the sum of indices of pairs that are in the right order', () => {
+    it('should parse lines into a `NumberArray` data structure', () => {
+        expect(parseLine('[1,1,3,1,1]')).toStrictEqual([1, 1, 3, 1, 1]);
+        expect(parseLine('[[1],[2,3,4]]')).toStrictEqual([[1], [2, 3, 4]]);
+        expect(parseLine('[]')).toStrictEqual([]);
+        expect(parseLine('[1,[2,[3,[4,[5,6,7]]]],8,9]')).toStrictEqual([
+            1,
+            [2, [3, [4, [5, 6, 7]]]],
+            8,
+            9,
+        ]);
+    });
+
+    it('should compare left and right NumberArrays and determine if they are in order', () => {
+        // Example scenarios in description
+        expect(compare([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])).toStrictEqual(1);
+        expect(compare([[1], [2, 3, 4]], [[1], 4])).toStrictEqual(1);
+        expect(compare([9], [[8, 7, 6]])).toStrictEqual(-1);
+        expect(compare([[4, 4], 4, 4], [[4, 4], 4, 4, 4])).toStrictEqual(1);
+        expect(compare([7, 7, 7, 7], [7, 7, 7])).toStrictEqual(-1);
+        expect(compare([], [3])).toStrictEqual(1);
+        expect(compare([[[]]], [[]])).toStrictEqual(-1);
+        expect(
+            compare(
+                [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+                [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+            ),
+        ).toStrictEqual(-1);
+        // Additional scenarios
+        expect(compare([2], [1])).toStrictEqual(-1);
+        expect(
+            compare(
+                [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+                [1, [2, [3, [4, [5, 6, 7]]]], 8, 7],
+            ),
+        ).toStrictEqual(-1);
+    });
+
+    it('should return the sum of indices of pairs that are in the right order', () => {
         expect(sumOfIndicesOfPairsInCorrectOrder(smallInput)).toStrictEqual(13);
-        // expect(sumOfIndicesOfPairsInCorrectOrder(puzzleInput)).toStrictEqual(1);
+        expect(sumOfIndicesOfPairsInCorrectOrder(puzzleInput)).toStrictEqual(5503);
+    });
+
+    it('should identify the decoder key for the distress signal', () => {
+        expect(findDecoderKey(smallInput)).toStrictEqual(140);
+        expect(findDecoderKey(puzzleInput)).toStrictEqual(20952);
     });
 });
--- src/2022/13-distress-signal/signals.ts
+++ src/2022/13-distress-signal/signals.ts
@@ -1,7 +1,100 @@
+type NumberArray = Array<number | NumberArray>;
+
+/* Destructuring for better readability */
+const isArray = Array.isArray;
+const isNumber = Number.isInteger;
+
+export function parseLine(line: string): NumberArray {
+    return JSON.parse(line);
+}
+
+export function compare(left: number | NumberArray, right: number | NumberArray): number {
+    if (isArray(left) && isNumber(right)) {
+        right = [right];
+    }
+
+    if (isNumber(left) && isArray(right)) {
+        left = [left];
+    }
+
+    if (isNumber(left) && isNumber(right)) {
+        if (left < right) {
+            return 1;
+        }
+        if (left === right) {
+            return 0;
+        }
+        return -1;
+    }
+
+    if (isArray(left) && isArray(right)) {
+        let i = 0;
+        while (i < left.length && i < right.length) {
+            const result = compare(left[i], right[i]);
+            if (result === 1) {
+                return 1;
+            }
+            if (result === -1) {
+                return -1;
+            }
+            i++;
+        }
+
+        if (i === left.length) {
+            if (left.length === right.length) {
+                return 0;
+            }
+            return 1;
+        }
+
+        return -1;
+    }
+}
+
 export function sumOfIndicesOfPairsInCorrectOrder(input: string): number {
-    const pairs = input.split('\n\n').map((pair) => pair.split('\n'));
+    const pairs = input.split('\n\n').map((pair) => pair.split('\n').map(parseLine));
 
-    console.log({ pairs });
+    let correctOrderCount = 0;
+    pairs.forEach(([left, right], index) => {
+        if (compare(left, right) === 1) {
+            correctOrderCount += index + 1;
+        }
+    });
 
-    return 0;
+    return correctOrderCount;
+}
+
+function isDividerPacket(value: NumberArray): value is [[number]] {
+    return (
+        value.length === 1 &&
+        isArray(value[0]) &&
+        value[0].length === 1 &&
+        isNumber(value[0][0])
+    );
+}
+
+export function findDecoderKey(input: string): number {
+    const packets = input
+        .split('\n')
+        .filter((line) => line !== '')
+        .map(parseLine);
+
+    packets.push([[2]], [[6]]);
+    packets.sort((a, b) => -compare(a, b));
+
+    let indexDividerPacketOne: number;
+    let indexDividerPacketTwo: number;
+
+    packets.forEach((packet, index) => {
+        if (isDividerPacket(packet)) {
+            if (packet[0][0] === 2) {
+                indexDividerPacketOne = index + 1;
+            }
+            if (packet[0][0] === 6) {
+                indexDividerPacketTwo = index + 1;
+            }
+        }
+    });
+
+    return indexDividerPacketOne * indexDividerPacketTwo;
 }

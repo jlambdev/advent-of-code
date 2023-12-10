b998675b30620939e4fe2538673c6ac80fb93ee6
Jonathan Lamb <jlamb.8153@gmail.com>
2023-12-10 16:40:59 +0100

    2023 day 9 both parts

--- src/2023/09-mirage-maintenance/history.test.ts
+++ src/2023/09-mirage-maintenance/history.test.ts
@@ -1,9 +1,14 @@
-import { partOne } from './history';
+import { partOne, partTwo } from './history';
 import { smallInput, puzzleInput } from './input';
 
 describe('Mirage Maintenance', () => {
     it('can sum all extrapolated (next) values for all value histories', () => {
         expect(partOne(smallInput)).toStrictEqual(114);
-        // expect(partOne(puzzleInput)).toStrictEqual(0);
+        expect(partOne(puzzleInput)).toStrictEqual(2043183816);
+    });
+
+    it('can sum of left-most extrapolated values', () => {
+        expect(partTwo(smallInput)).toStrictEqual(2);
+        expect(partTwo(puzzleInput)).toStrictEqual(1118);
     });
 });
--- src/2023/09-mirage-maintenance/history.ts
+++ src/2023/09-mirage-maintenance/history.ts
@@ -1,4 +1,27 @@
+function predictNextValue(numbers: Array<number>): number {
+    if (numbers.every((n) => n === 0)) {
+        return 0;
+    }
+
+    const differences: Array<number> = [];
+    for (let i = 0; i < numbers.length - 1; i++) {
+        differences.push(numbers[i + 1] - numbers[i]);
+    }
+    return numbers[numbers.length - 1] + predictNextValue(differences);
+}
+
 export function partOne(input: string): number {
-    // some sort of recurion is required
-    return -1;
+    const histories = input
+        .split('\n')
+        .map((line: string) => line.split(' ').map(Number));
+
+    return histories.reduce((sum, history) => (sum += predictNextValue(history)), 0);
+}
+
+export function partTwo(input: string): number {
+    const histories = input
+        .split('\n')
+        .map((line: string) => line.split(' ').map(Number).reverse());
+
+    return histories.reduce((sum, history) => (sum += predictNextValue(history)), 0);
 }
